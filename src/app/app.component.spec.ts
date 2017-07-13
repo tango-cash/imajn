ae6e9e25b8b2fe074a4d64e7a3b2f149fdbb8e52
jwcnewton <jwcnewton@gmail.com>
2017-07-13 23:45:24 +0100

    Routing and about cleaned and moved scss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
+/* tslint:disable:no-unused-variable */
 import { TestBed, async } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
       ],
     }).compileComponents();
   }));
@@ -16,17 +20,18 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
 });
+
+//   it(`should have as title 'app'`, async(() => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app.title).toEqual('app');
+//   }));
+
+//   it('should render title in a h1 tag', async(() => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.debugElement.nativeElement;
+//     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+//   }));
+// });
